cb2d9fe5966bd7aefe1568ff4c0f5f8a8ae20d5b
sgibbes <sgibbes@wri.org>
2017-07-03 10:48:15 -0400

    extract bouding coords from 10deg tile name

---
+++ coords_from_tilename.py
@@ -0,0 +1,19 @@
+def coords(tile_id):
+    NS = tile_id.split("_")[0][-1:]
+    EW = tile_id.split("_")[1][-1:]
+
+    if NS == 'S':
+        ymax =-1*int(tile_id.split("_")[0][:2])
+    else:
+        ymax = int(str(tile_id.split("_")[0][:2]))
+    
+    if EW == 'W':
+        xmin = -1*int(str(tile_id.split("_")[1][:3]))
+    else:
+        xmin = int(str(tile_id.split("_")[1][:3]))
+        
+    
+    ymin = str(int(ymax) - 10)
+    xmax = str(int(xmin) + 10)
+    
+    return ymax, xmin, ymin, xmax
