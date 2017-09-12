460559c0ba0b3c7c09ee1b9c7c568f9f4ac206a7
sgibbes <sgibbes@wri.org>
2017-09-12 12:54:03 -0400

    first commits

---
+++ split_raster.py
@@ -0,0 +1,19 @@
+import os, sys
+from osgeo import gdal
+
+dset = gdal.Open(sys.argv[1])
+
+width = dset.RasterXSize
+height = dset.RasterYSize
+
+print width, 'x', height
+
+tilesize = 5000
+
+for i in range(0, width, tilesize):
+    for j in range(0, height, tilesize):
+        w = min(i+tilesize, width) - i
+        h = min(j+tilesize, height) - j
+        gdaltranString = "gdal_translate -of GTIFF -srcwin "+str(i)+", "+str(j)+", "+str(w)+", " \
+            +str(h)+" " + sys.argv[1] + " " + sys.argv[2] + "_"+str(i)+"_"+str(j)+".tif"
+        os.system(gdaltranString)
