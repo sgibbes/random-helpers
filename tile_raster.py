f1b27993751236561fcfc0b06f5bc4b8c88c8005
sgibbes <sgibbes@wri.org>
2017-09-18 13:20:08 -0400

    code to tile a raster into 10x10 deg seperate rasters

---
+++ tile_raster.py
@@ -0,0 +1,33 @@
+import gdal
+from gdalconst import GA_ReadOnly
+import subprocess
+# loop through tile ids to make individual rasters for each 10x10 deg tile id
+
+def coords(tile_id):
+    NS = tile_id.split("_")[0][-1:]
+    EW = tile_id.split("_")[1][-1:]
+
+    if NS == 'S':
+        ymax =-1*int(tile_id.split("_")[0][:2])
+    else:
+        ymax = int(str(tile_id.split("_")[0][:2]))
+    
+    if EW == 'W':
+        xmin = -1*int(str(tile_id.split("_")[1][:3]))
+    else:
+        xmin = int(str(tile_id.split("_")[1][:3]))
+        
+    
+    ymin = str(int(ymax) - 10)
+    xmax = str(int(xmin) + 10)
+    
+    return ymax, xmin, ymin, xmax
+    
+def clip_tiles(tile_id):
+
+    out =  r'{}_tiled.tif'.format(tile_id)
+    input_raster = 'your_raster.tif'
+
+    ymax, xmin, ymin, xmax = coords(tile_id)
+    cmd = ['gdalwarp', '-tr', '.00025', '.00025',  '-co', 'COMPRESS=LZW', '-tap', input_raster, out, '-te', str(xmin), str(ymin), str(xmax), str(ymax)]  
+    subprocess.check_call(cmd)
